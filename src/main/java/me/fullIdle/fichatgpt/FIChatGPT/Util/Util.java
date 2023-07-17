cecced77a18ed9457c4351023495e68d7e4e3c00
master <2768244699@qq.com>
2023-07-17 23:31:23 +0800

    修整连续对话

--- src/main/java/me/fullIdle/fichatgpt/FIChatGPT/Util/Util.java
+++ src/main/java/me/fullIdle/fichatgpt/FIChatGPT/Util/Util.java
@@ -84,7 +84,7 @@ public class Util {
                     }
                     int delay = config.getInt("ContinuousDialogue");
                     if (player != null)if (delay > 0){
-                        setContinuousRunnable(player);
+                        setContinuousRunnable(player,delay);
                     }
                     String replay = getMsg(config.getString("Format"), getReplay(jsonString));
                     if (player != null) {
@@ -105,7 +105,7 @@ public class Util {
         };
     }
 
-    public static BukkitRunnable setContinuousRunnable(Player player){
+    public static BukkitRunnable setContinuousRunnable(Player player,int daley){
         BukkitRunnable runnable = continuousRunnable.get(player.getUniqueId());
         if (runnable != null){
             runnable.cancel();
@@ -118,7 +118,7 @@ public class Util {
             }
         };
         continuousRunnable.put(player.getUniqueId(),runnable);
-        runnable.run();
+        runnable.runTaskLater(main,20*daley);
         return runnable;
     }
 }
